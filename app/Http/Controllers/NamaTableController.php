ddd84d90381cc2df9028a596a686da44b249aaca
daparamadhan <daparamdnnn@gmail.com>
2025-05-19 17:12:24 +0700

    set up

---
+++ app/Http/Controllers/NamaTableController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\nama_table;
+use Illuminate\Http\Request;
+
+class NamaTableController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(nama_table $nama_table)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(nama_table $nama_table)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, nama_table $nama_table)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(nama_table $nama_table)
+    {
+        //
+    }
+}
